b71d04b3976060dfdeb959fadd2a6bb823895909
William Kamp <manimaul@gmail.com>
2023-11-07 22:32:01 -0800

    people table

--- src/jsMain/kotlin/components/routes/People.kt
+++ src/jsMain/kotlin/components/routes/People.kt
@@ -3,10 +3,13 @@ package components.routes
 import androidx.compose.runtime.*
 import com.mxmariner.regatta.data.Person
 import components.Confirm
+import components.RgButton
+import components.RgButtonStyle
 import components.Spinner
 import org.jetbrains.compose.web.attributes.InputType
 import org.jetbrains.compose.web.attributes.name
 import org.jetbrains.compose.web.attributes.placeholder
+import org.jetbrains.compose.web.css.CSSUnit
 import org.jetbrains.compose.web.dom.*
 import viewmodel.DeletePerson
 import viewmodel.PeopleStateLoaded
@@ -18,44 +21,8 @@ fun People(
     viewModel: PeopleViewModel = remember { PeopleViewModel() }
 ) {
     Div {
-        H4 {
-            Text("People")
-        }
         when (val state = viewModel.state) {
-            is PeopleStateLoaded -> {
-                state.people.takeIf { it.isNotEmpty() }?.let { people ->
-                    Table {
-                        Tr {
-                            Th { Text("First") }
-                            Th { Text("Last") }
-                            Th { Text("Boat") }
-                            Th { Text("Member") }
-                            Th { Text("") }
-                        }
-                        people.forEach { person ->
-                            Tr {
-                                Td { Text(person.first) }
-                                Td { Text(person.last) }
-                                Td { Text("-") }
-                                Td { Text(if(person.clubMember) "Yes" else "No") }
-                                Td {
-                                    Button(attrs = {
-                                        onClick { viewModel.setDeletePerson(person) }
-                                    }) {
-                                        Text("X")
-                                    }
-                                }
-                            }
-                        }
-                    }
-                } ?: run {
-                    P { Text("No one was found") }
-                }
-                Br()
-                Hr()
-                H4 { Text("Add person") }
-                AddPerson(viewModel)
-            }
+            is PeopleStateLoaded -> PeopleLoaded(state, viewModel)
             PeopleStateLoading -> Spinner(50f)
             is DeletePerson -> {
                 Confirm("Delete '${state.person.first} ${state.person.last}'?") { delete ->
@@ -71,42 +38,79 @@ fun People(
 }
 
 @Composable
+fun PeopleLoaded(
+    state: PeopleStateLoaded,
+    viewModel: PeopleViewModel,
+) {
+    Div {
+        Article {
+            H1 { Text("People") }
+        }
+        Table(attrs = { classes("striped") }) {
+            state.people.takeIf { it.isNotEmpty() }?.let { people ->
+                Tr {
+                    Th { Text("First") }
+                    Th { Text("Last") }
+                    Th { Text("Boat") }
+                    Th { Text("Member") }
+                    Th { Text("Action") }
+                }
+                people.forEach { person ->
+                    Tr {
+                        Td { Text(person.first) }
+                        Td { Text(person.last) }
+                        Td { Text("-") }
+                        Td { Text(if (person.clubMember) "Yes" else "No") }
+                        Td {
+                            RgButton("Delete", RgButtonStyle.Error) {
+                                viewModel.setDeletePerson(person)
+                            }
+                        }
+                    }
+                }
+                AddPerson(viewModel)
+            }
+        }
+    }
+}
+
+@Composable
 fun AddPerson(viewModel: PeopleViewModel) {
     var first by remember { mutableStateOf("") }
     var last by remember { mutableStateOf("") }
     var member by remember { mutableStateOf(false) }
-    Input(type = InputType.Text) {
-        placeholder("First")
-        onInput {
-            first = it.value
+    Tr {
+        Td {
+            Input(type = InputType.Text) {
+                placeholder("First")
+                onInput {
+                    first = it.value
+                }
+                value(first)
+            }
         }
-        value(first)
-    }
-    Br()
-    Input(type = InputType.Text) {
-        placeholder("Last")
-        onInput {
-            last = it.value
+        Td {
+            Input(type = InputType.Text) {
+                placeholder("Last")
+                onInput {
+                    last = it.value
+                }
+                value(last)
+            }
         }
-        value(last)
-    }
-    CheckboxInput {
-        id("Member")
-        name("Member")
-        onChange {
-            member = it.value
+        Td {  }
+        Td {
+            CheckboxInput {
+                onChange {
+                    member = it.value
+                }
+                checked(member)
+            }
         }
-        checked(member)
-    }
-    Label("Member") { Text("Club member") }
-    if (first.isNotBlank() && last.isNotBlank()) {
-        Br()
-        Button(attrs = {
-            onClick {
+        Td {
+            RgButton("Add", RgButtonStyle.Primary, first.isBlank() || last.isBlank()) {
                 viewModel.upsertPerson(Person(first = first, last = last, clubMember = member))
             }
-        }) {
-            Text("Add")
         }
     }
 }
