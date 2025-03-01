afc586102d667ff2f67ecbc276b5ea4e1e25b283
William Kamp <manimaul@gmail.com>
2025-03-01 07:29:10 -0800

    fix jsRun dev server

--- web/webpack.config.d/dev_server_config.js
+++ web/webpack.config.d/dev_server_config.js
@@ -1,6 +1,13 @@
+const path = require('path');
+
 config.devServer.proxy = {
-     '/v1/api': 'http://localhost:8888'
+    '/v1/api': 'http://localhost:8888',
 }
 config.devServer.historyApiFallback = {
-     index: 'index.html',
+    index: 'index.html',
+}
+
+// //root = <project>/build/js/packages/regatta-web
+config.devServer.static = {
+    directory: path.join(__dirname, '../../../../common/src/commonMain/resources/static'),
 }
