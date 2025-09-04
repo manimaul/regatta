6c99e29db009ebc7e76c0b5292da45b1a513c1c3
William Kamp <manimaul@gmail.com>
2025-09-03 18:28:00 -0700

    size broadcast

--- web/src/jsMain/kotlin/Main.js.kt
+++ web/src/jsMain/kotlin/Main.js.kt
@@ -1,10 +1,35 @@
+import androidx.compose.runtime.DisposableEffectResult
+import kotlinx.browser.window
+import org.jetbrains.compose.web.dom.Div
 import org.jetbrains.compose.web.renderComposable
+import utils.ResizeObserver
 
 external fun require(module: String): dynamic
+
+val resizeHandler = ResizeObserver { entries, observer ->
+    val wh = window.innerHeight
+    val h = entries[0].contentBoxSize[0].blockSize.toInt()
+    println("resize wh=$wh h = $h")
+    window.parent.postMessage(
+        JSON.parse("{\"regatta_scroll_height\": $h}"),
+        "*"
+    )
+}
 
 fun main() {
     require("maplibre-gl/dist/maplibre-gl.css")
     require("bootstrap/dist/css/bootstrap.css")
     require("bootstrap/dist/js/bootstrap.bundle.js")
-    renderComposable(rootElementId = "root") { Router() }
+    renderComposable(rootElementId = "root") {
+        Div(attrs = {
+            ref {
+                resizeHandler.observe(it)
+                object : DisposableEffectResult {
+                    override fun dispose() {
+                        resizeHandler.unobserve(it)
+                    }
+                }
+            }
+        }) { Router() }
+    }
 }
