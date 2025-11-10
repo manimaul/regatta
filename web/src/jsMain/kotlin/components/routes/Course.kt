fd3d92a431c4938dd7c4e08500a133ba22577fa9
William Kamp <manimaul@gmail.com>
2025-11-09 16:48:42 -0800

    race marks dropdown

--- web/src/jsMain/kotlin/components/routes/Course.kt
+++ web/src/jsMain/kotlin/components/routes/Course.kt
@@ -2,11 +2,29 @@ package components.routes
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffectResult
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import components.RgDropdown
 import org.jetbrains.compose.web.dom.Div
+import styles.AppStyle
 import utils.chartViewModel
+import utils.marks
 
 @Composable
 fun Course() {
+    val state by chartViewModel.flow.collectAsState()
+
+    Div(attrs = {
+        classes(AppStyle.marginBot)
+    }) {
+        RgDropdown(
+            items = marks,
+            selectedItem = state.mark,
+            name = { "${it?.letter} ${it?.name} - ${it?.desc}" },
+            handler = {
+                chartViewModel.selectMark(it)
+            })
+    }
     Div(
         attrs = {
             classes("flex-grow-1")
@@ -18,5 +36,6 @@ fun Course() {
                     }
                 }
             }
-        })
+        }
+    )
 }
